8e494d6ebec280cf7fd014624281cd27d9b5e63f
alex.zeng <595276686@qq.com>
2020-09-29 14:46:22 +0800

    feat: first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# umi-plugin-cordova-enhance
+
+[![NPM version](https://img.shields.io/npm/v/umi-plugin-cordova-enhance.svg?style=flat)](https://npmjs.org/package/umi-plugin-cordova-enhance)
+[![NPM downloads](http://img.shields.io/npm/dm/umi-plugin-cordova-enhance.svg?style=flat)](https://npmjs.org/package/umi-plugin-cordova-enhance)
+
+
+
+## Install
+
+```bash
+# or yarn
+$ npm install
+```
+
+```bash
+$ npm run build --watch
+$ npm run start
+```
+
+## Usage
+
+Configure in `.umirc.js`,
+
+```js
+export default {
+  plugins: [
+    ['umi-plugin-umi-plugin-cordova-enhance', options],
+  ],
+}
+```
+
+## Options
+
+TODO
+
+## LICENSE
+
+MIT
---
+++ src/utils/config.js
@@ -0,0 +1,93 @@
+function parseConfig(options, outputPath) {
+  const baseXml = fs.readFileSync(defaultOptions.baseConfigPath, "utf-8");
+  const config = JSON.parse(parser.toJson(baseXml, { reversible: true }));
+  const { widget } = config;
+  const finalWidget = writePermissions(
+    writeIconsAndSplashes(writeInfos(widget, options), options),
+    options
+  );
+  fs.writeFileSync(
+    outputPath || options.configPath,
+    format(parser.toXml({ ...config, widget: finalWidget }))
+  );
+}
+
+function writeIconsAndSplashes(widget, { resPath, env }) {
+  const parsedPath = path.resolve(resPath);
+  const res = parseIconsAndSplashes(parsedPath, env);
+  const { platform, ...restConfig } = widget;
+  const { android: resAndroid, iOS: resApple } = res;
+  const parsedPlatforms = [];
+  const resList = [resAndroid, resApple];
+  const dirName = process.cwd();
+  resList.forEach(({ icons, splashes }, index) => {
+    const targetPlatform = platform[index];
+    targetPlatform.icon = icons;
+    targetPlatform.splash = splashes;
+    parsedPlatforms.push(targetPlatform);
+  });
+  return {
+    ...restConfig,
+    platform: parsedPlatforms
+  };
+}
+
+function writePermissions(widget, { permissions }) {
+  const permissionList = parsePermissions(permissions);
+  const { platform, ...restConfig } = widget;
+  const platformIOS = platform[1];
+  if (!platformIOS["config-file"]) {
+    platformIOS["config-file"] = [];
+  }
+  permissionList.forEach(permission => {
+    const { permissionType, desc, parent } = permission;
+    if (permissionType === "config-file") {
+      platformIOS["config-file"].push({
+        parent,
+        platform: "ios",
+        target: "*-Info.plist",
+        string: {
+          $t: desc
+        }
+      });
+    }
+    if (permissionType === "edit-config") {
+      platformIOS["edit-config"] = {
+        file: "*-Info.plist",
+        mode: "merge",
+        target: parent,
+        string: {
+          $t: desc
+        }
+      };
+    }
+  });
+  platform[1] = platformIOS;
+  return {
+    ...restConfig,
+    platform
+  };
+}
+
+function writeInfos(widget, options) {
+  const finalWidget = { ...widget };
+  const {
+    locale = "zh_CN",
+    id,
+    version,
+    name,
+    description,
+    author: { email, href, name: authorName }
+  } = options;
+  finalWidget.defaultLocale = locale;
+  finalWidget.id = id;
+  finalWidget.version = version;
+  finalWidget.name["$t"] = name;
+  finalWidget.description["$t"] = description;
+  finalWidget.author = {
+    email,
+    href,
+    $t: authorName
+  };
+  return finalWidget;
+}
