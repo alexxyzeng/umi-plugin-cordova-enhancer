8f7b3d32cdc852e8f0127de8b99e8c91541a8d78
alex.zeng <595276686@qq.com>
2020-10-12 17:56:12 +0800

    feat: update plugin typings

--- example/pages/index.js
+++ example/pages/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import styles from './index.css';
+import React from "react";
+import styles from "./index.css";
 
 export default function() {
-  return (
+  window.cordova.file.return(
     <div className={styles.normal}>
       <h1>Page index</h1>
     </div>
